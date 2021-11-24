762bd26a25e1b0f92fe42e8f1ef4c013319d9d84
MR3730415 <m3n7m3.97.415@gmail.com>
2021-11-24 16:25:22 +0000

    [update]

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,24 +1,3 @@
 class Admin::OrderDetailsController < ApplicationController
-    before_action :authenticate_admin!
-
-  def show
-    @order = Order.find(params[:id])
-    @customer = @order.customer
-    @order_details = @order.order_details
-  end
-
-  def update
-    @order = Order.find(params[:id])
-    @order.update(order_params)
-    if @order.status == "confirm"
-      @order.order_details.update(making: "wait")
-    end
-    redirect_to admin_order_path(@order.id)
-  end
-
-  private
-
-  def order_params
-    params.require(:order).permit(:status)
-  end
+    
 end
